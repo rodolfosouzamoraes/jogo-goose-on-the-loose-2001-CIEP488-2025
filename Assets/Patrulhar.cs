dcfe78b04c6ac8e6b6dfd5febb13a4e9cb99d600
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2025-10-22 09:01:20 -0300

    Ajuste Inimigo Patrulheiro

--- Assets/Patrulhar.cs
+++ Assets/Patrulhar.cs
@@ -1,22 +1,22 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Patrulhar : MonoBehaviour
 {
-    [SerializeField] GameObject corpoPatrulheiro;
-    [SerializeField] float velocidadeMovimento;
-    [SerializeField] float velocidadeRotacao;
-    [SerializeField] MoverParaFrente moverObjeto;
+    [SerializeField] private GameObject corpoPatrulheiro;
+    [SerializeField] private float velocidadeMovimento = 5f;
+    [SerializeField] private float velocidadeRotacao = 2f;
+    [SerializeField] private MoverParaFrente moverObjeto;
+
     private bool estaMovendo = true;
     private bool estaRotacionando = false;
-    private float angulo;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float anguloAtual; // ângulo real em graus
+
     void Start()
     {
-        angulo = corpoPatrulheiro.transform.rotation.y;
+        // Inicializa com o ângulo Y real do objeto (em graus)
+        anguloAtual = corpoPatrulheiro.transform.eulerAngles.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Rotacionar();
@@ -24,21 +24,21 @@ public class Patrulhar : MonoBehaviour
 
     private void Rotacionar()
     {
-        if (estaRotacionando == false) return;
-        Vector3 direcaoObjeto = corpoPatrulheiro.transform.forward;
-        Vector3 rotacaoAlvo = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
-        Vector3 novaRotacao = Vector3.RotateTowards(
-            direcaoObjeto,
+        if (!estaRotacionando) return;
+
+        // Calcula a rotação alvo em graus
+        Quaternion rotacaoAlvo = Quaternion.Euler(0, anguloAtual, 0);
+
+        // Faz interpolação suave até o ângulo alvo
+        corpoPatrulheiro.transform.rotation = Quaternion.RotateTowards(
+            corpoPatrulheiro.transform.rotation,
             rotacaoAlvo,
-            velocidadeRotacao * Time.deltaTime,
-            0f
+            velocidadeRotacao * Time.deltaTime
         );
 
-        corpoPatrulheiro.transform.rotation = Quaternion.LookRotation(novaRotacao);
-
-        float diferencaAngulo = Vector3.Angle(corpoPatrulheiro.transform.forward, rotacaoAlvo) * Mathf.Deg2Rad;
-
-        if (diferencaAngulo <= 0.01f)
+        // Verifica se chegou próximo o suficiente do alvo
+        float diferenca = Quaternion.Angle(corpoPatrulheiro.transform.rotation, rotacaoAlvo);
+        if (diferenca <= 0.1f)
         {
             estaRotacionando = false;
             moverObjeto.InverterVelocidade();
@@ -47,7 +47,13 @@ public class Patrulhar : MonoBehaviour
 
     public void PermitirRotacionar()
     {
-        angulo += 180;
+        // Incrementa a rotação em 180 graus
+        anguloAtual += 180f;
+
+        // Mantém o valor entre 0 e 360
+        anguloAtual %= 360f;
+
+        // Ativa a rotação e pausa o movimento
         estaRotacionando = true;
         moverObjeto.PararMovimentacao();
     }
